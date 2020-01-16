f7b48bb339438823d62308bdac16b89b66ba0421
Rolando <rolandosumoza@gmail.com>
2020-01-16 10:43:30 -0600

    Actividad

---
+++ playground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+/* Instructions */
+var v1 = "Esto es swift"
+let c1 = 4
+var cadena: String = String()
+
+cadena = "Hola mundo " + v1
+
+print("El contenido es: \(cadena) ")
+print("El valor de la constante es: \(c1) ")
+
